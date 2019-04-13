98dc5496d8931936f1cb9a523328f6d712238279
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-04-13 12:05:08 +0900

    discardableResultについて追記

--- chapter_from_4.playground/Contents.swift
+++ chapter_from_4.playground/Contents.swift
@@ -14,6 +14,24 @@ PlaygroundPage.current.liveView = views[0] as! NSView
 
 print("hello")
 
+func get_double_value(x: Double) -> Double{
+    return x*2.0
+}
+
+func get_double_value2(_ x: Int)->Int{//呼び出し時に引数名を省略させる(むしろ引数名をつけることができなくなる)には_ をつける
+    return x * 2
+}
+
+print(get_double_value(x: 4.2))
+print(get_double_value2(2))
 
 
+//通常は戻り値を無視すると警告がコンパイラから発せられるが，discardableResult属性をつけるとそれを抑制することができる
+@discardableResult
+func get_discardableResult()->Double{
+    return 1.0
+}
+
+get_discardableResult()
+_ = get_discardableResult() // _で受け取ると明示的に結果を捨てることを示すことができる
 
