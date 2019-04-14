99470a63f5b9b901bceb38f6295449023fa937d4
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-04-14 19:26:59 +0900

    enum

--- chapter678.playground/Contents.swift
+++ chapter678.playground/Contents.swift
@@ -20,9 +20,58 @@ struct chapter6{
     // letなどのプロパティは初期化時に必ず初期化されていること
     
     // 自動的にinit(v, x)が定義される
+    
+}
+
+class SampleClass{
+    
+    deinit{
+        // 下位クラスから順次呼び出される
+    }
 }
 
 let c6 = chapter6(v:3, x:2)
+
+let sampleClass = SampleClass()
+
+
+enum Week{
+    case Sunday
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+    
+    init?(_ name: String){
+        switch name{
+        case "日":
+            self = .Sunday
+        default:
+            return nil
+        }
+        
+    }
+
+    var name: String{
+        switch self{
+        case .Sunday: return "日"
+        case .Monday: return "月"
+        case .Tuesday: return "火"
+        case .Wednesday: return "水"
+        case .Thursday: return "木"
+        case .Friday: return "金"
+        case .Saturday: return "土"
+        case .Sunday: return "日"
+        
+        }
+        
+    }
+
+}
+
+
 
 
 
